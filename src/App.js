238a6f09bdda00400d2cd02ee062e705e2126778
Faith M. Roberts <faithroberts708@gmail.com>
2022-10-28 19:27:18 +0100

    adding registration form

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Registration from './components/Registration';
+
+function App() {
+  return (
+    <div className="App">
+      <Registration />
+    </div>
+  );
+}
+
+export default App;
